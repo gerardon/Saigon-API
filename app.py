fca3f45deb838a4d9ab46a7a3e3616e31891ee7c
Raphael Passini <raphapassini@gmail.com>
2016-10-23 21:14:49 -0200

    Encapsulate api setup and route creation

--- app.py
+++ app.py
@@ -1,11 +1,20 @@
 import falcon
 from resources import (BoardResource, StartTurnResource, VoteResource,
-                       NewGameResource)
+                       NewGameResource, ComputeVotesResource)
 
 
-api = falcon.API()
-api.req_options.auto_parse_form_urlencoded = True
-api.add_route('/board', BoardResource())
-api.add_route('/start_turn', StartTurnResource())
-api.add_route('/vote', VoteResource())
-api.add_route('/new_game', NewGameResource())
+def api_setup(api):
+    api.req_options.auto_parse_form_urlencoded = True
+    return api
+
+
+def api_set_routes(api):
+    api.add_route('/board', BoardResource())
+    api.add_route('/start_turn', StartTurnResource())
+    api.add_route('/vote', VoteResource())
+    api.add_route('/new_game', NewGameResource())
+    api.add_route('/compute_votes', ComputeVotesResource())
+    return api
+
+api = api_setup(falcon.API())
+api = api_set_routes(api)
