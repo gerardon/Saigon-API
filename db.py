11c3c40dbb26b7fe39048cc99d0c20bd6fb70df5
Raphael Passini <raphapassini@gmail.com>
2016-10-23 23:36:35 -0200

    Add db function to know who is the winner

--- db.py
+++ db.py
@@ -55,6 +55,11 @@ class GameRepository(BaseRepository):
         counter = Counter([b['alignment'] for b in words])
         return counter.most_common()[0][0]
 
+    @classmethod
+    def get_word(cls, word, words):
+        _word = list(filter(lambda x: x['word'] == word, words))
+        return _word[0] if _word else ''
+
     def new(self, start_team, words):
         # remove o jogo antigo
         self.delete()
@@ -82,7 +87,7 @@ class GameRepository(BaseRepository):
         if game:
             voting_expires_at = game.get('voting_expires_at')
             if voting_expires_at and voting_expires_at < datetime.now():
-                self.count_votes(game['votes'])
+                self.compute_votes()
 
         return game
 
@@ -106,28 +111,55 @@ class GameRepository(BaseRepository):
         total_votes = len(votes)
         counter = Counter(votes)
 
-        if not counter:
+        if not counter or counter[''] > total_votes // 2:
             return
 
-        if counter[''] > total_votes // 2:
-            self.end_turn()
-        else:
-            word, votes = counter.most_common()[0]
-
-            word_repository = WordRepository()
-            word_repository.publish_word(word)
-            words = word_repository.all()
-            game = self.retrieve_one()
-            game['words'] = words
-            self.replace(game)
+        word, votes = counter.most_common()[0]
+        word_repository = WordRepository()
+        word_repository.publish_word(word)
+        words = word_repository.all()
+        game = self.retrieve_one()
+        game['words'] = words
+        self.replace(game)
+        return word
 
     def compute_votes(self, game=None):
         game = self.retrieve_one(game or {})
         if not game:
             return
 
-        self.count_votes(game['votes'])
+        word = self.count_votes(game['votes'])
+        if word and self.word_is_from_team(word, game['turn']):
+            return
         self.end_turn()
+        self.who_is_the_winner()
+
+    def word_is_from_team(self, word, team):
+        wr = WordRepository()
+        _word = wr.retrieve_one(word)
+        return _word['alignment'] == team
+
+    def who_is_the_winner(self, game=None):
+        game = game or {}
+        game = self.retrieve_one(game)
+        if not game:
+            return
+
+        wr = WordRepository()
+        blue_words = wr.collection.find({'alignment': 'blue'})
+        red_words = wr.collection.find({'alignment': 'red'})
+
+        guessed_blues = list(filter(lambda x: x['public'], blue_words))
+        guessed_red = list(filter(lambda x: x['public'], red_words))
+        if len(guessed_blues) == blue_words.count():
+            winner = 'blue'
+        elif len(guessed_red) == red_words.count():
+            winner = 'red'
+        else:
+            return
+
+        self.collection.update_one(game, {'$set': {'winner': winner}})
+        return winner
 
 
 class WordRepository(BaseRepository):
